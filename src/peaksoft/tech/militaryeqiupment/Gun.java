c3cc41e36c126402a80b862d85a48d7d905996ee
Beksultan Mamatkadyr uulu <90905002+beksultanFromPeaksoft@users.noreply.github.com>
2022-02-11 19:11:44 +0600

    Update Gun.java

--- src/peaksoft/tech/militaryeqiupment/Gun.java
+++ src/peaksoft/tech/militaryeqiupment/Gun.java
@@ -2,6 +2,8 @@ package peaksoft.tech.militaryeqiupment;
 
 public class Gun extends MilitaryEquipment {
     private String typeOfBullet;
+    
+    //DEFINE CONSTRUCTOR WITHOUT ARGUMENTS
 
     public Gun(String nameTech, String colorTech, double mass, double price, int firingRange, String material, String typeOfBullet) {
         super(nameTech, colorTech, mass, price, firingRange, material);
